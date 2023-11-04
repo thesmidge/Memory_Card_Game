ab5832216dbb88058762622fab0e44370f93122c
Alicia Jones <smidge155@gmail.com>
2023-11-04 16:07:32 -0600

    Shuffle cardIndexArray with Lodash

--- src/features/gameBoard/GameBoard.js
+++ src/features/gameBoard/GameBoard.js
@@ -2,23 +2,15 @@ import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Container, Row, Col } from 'reactstrap';
 import MemoryCard from '../memoryCards/MemoryCard';
+import _ from 'lodash';
 
 const GameBoard = () => {
     // const memoryCards = useSelector((state) => state.memoryCards.memoryCardsArray);
 
-    const [cardIndexArray, setCardIndexArray] = useState([
-        0, 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21
-    ]);
+    const cardIndexArray = [0, 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+    const shuffledCardIndexArray = _.shuffle(cardIndexArray);
     console.log('Beginning index array: ' + cardIndexArray);
-
-    const shuffleArray = (array) => {
-        const shuffledArray = array.toSorted(() => 0.5 - Math.random());
-        return shuffleArray;
-    };
-
-    setCardIndexArray(shuffleArray(cardIndexArray));
-
-    console.log('Shuffled index array: ' + cardIndexArray);
+    console.log('Shuffled index array: ' + shuffledCardIndexArray);
 
     return (
         <Container>
