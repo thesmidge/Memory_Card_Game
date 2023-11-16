eb86141f15e8f5f45b98e23f279e063613b68abd
Alicia Jones <smidge155@gmail.com>
2023-11-15 21:59:35 -0700

    Move Shuffling of Card Deck to GamePage

--- src/features/gameBoard/GameBoard.js
+++ src/features/gameBoard/GameBoard.js
@@ -4,15 +4,8 @@ import { Container, Row, Col } from 'reactstrap';
 import MemoryCard from '../memoryCards/MemoryCard';
 import _ from 'lodash';
 
-const GameBoard = () => {
-    const cardIndexArray = [0, 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
-    const shuffledCardIndexArray = _.shuffle(cardIndexArray);
-    const truncatedCardIndexArray = shuffledCardIndexArray.splice(0, 10);
-    const duplicatedIndexArray = truncatedCardIndexArray.concat(
-        truncatedCardIndexArray
-    );
-    const memoryCardIndexArray = _.shuffle(duplicatedIndexArray);
-
+const GameBoard = ({ memoryCardIndexArray }) => {
+    
     return (
         <Container>
             <h1>Some Title</h1>
--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,9 +1,18 @@
 import GameBoard from '../features/gameBoard/GameBoard';
-import { shuffleMemoryCards } from '../features/memoryCards/memoryCardsSlice';
+import _ from 'lodash';
 
 const GamePage = () => {
+    const cardIndexArray = [0, 1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+    const shuffledCardIndexArray = _.shuffle(cardIndexArray);
+    const truncatedCardIndexArray = shuffledCardIndexArray.splice(0, 10);
+    const duplicatedIndexArray = truncatedCardIndexArray.concat(
+        truncatedCardIndexArray
+    );
+    const memoryCardIndexArray = _.shuffle(duplicatedIndexArray);
 
-    return <GameBoard />;
+    return (
+        <GameBoard memoryCardIndexArray={memoryCardIndexArray} />
+    );
 };
 
 export default GamePage;
